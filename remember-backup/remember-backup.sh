3cdeda14338790c9afad57b5e104039d37442d40
Pablo Cases <cases.pablo@gmail.com>
2015-05-20 21:40:53 +0200

    improved user feedback during execution improved pre-requisite checks during execution added template for passwordless (no askpass program needed) execution of rsync started inclusion of backup rotation

--- remember-backup/remember-backup.sh
+++ remember-backup/remember-backup.sh
@@ -12,39 +12,92 @@ fi
 : ${REMEMBER_ONSITE_REVERSE_PORT:=2222}
 : ${REMEMBER_OFFSITE_USER:=root}
 
+# check that the SSH tunnel exists
 if ! ssh -p$REMEMBER_ONSITE_REVERSE_PORT $REMEMBER_OFFSITE_USER@localhost exit; then
     echo " FAIL"
     echo "Reverse SSH tunnel not active. Abort."
     exit 1
 fi
 
+echo -n "Check local directory..."
 if [ ! -d $REMEMBER_SOURCE ]; then
     echo " FAIL"
-    echo "Missing directory \"$REMEMBER_SOURCE\". Abort."
+    echo "Missing local directory \"$REMEMBER_SOURCE\". Abort."
     exit 1
 fi
+echo " OK"
 
-if [ "not" == "$(ssh -p$REMEMBER_ONSITE_REVERSE_PORT $REMEMBER_OFFSITE_USER@localhost [ ! -d $REMEMBER_DEST ] && echo "not")" ]; then
+#TODO: decrypt backup at some early stage
+
+echo -n "Check remote base directory..."
+if ssh -p$REMEMBER_ONSITE_REVERSE_PORT $REMEMBER_OFFSITE_USER@localhost "[ ! -d $REMEMBER_BASEPATH ]"; then
     echo " FAIL"
-    echo "Missing directory \"$REMEMBER_DEST\". Abort."
+    echo "Missing remote directory \"$REMEMBER_BASEPATH\". Abort."
     exit 1
 fi
+echo " OK"
 
-if [ "not" == "$(ssh -p$REMEMBER_ONSITE_REVERSE_PORT $REMEMBER_OFFSITE_USER@localhost [ ! -d $REMEMBER_LINKDEST ] && echo "not")" ]; then
+echo -n "Check remote destination directory (current)..."
+REMEMBER_DEST=$REMEMBER_BASEPATH/current
+if ssh -p$REMEMBER_ONSITE_REVERSE_PORT $REMEMBER_OFFSITE_USER@localhost "[ ! -d $REMEMBER_DEST ]"; then
+    if ! ssh -p$REMEMBER_ONSITE_REVERSE_PORT $REMEMBER_OFFSITE_USER@localhost "mkdir $REMEMBER_DEST"; then
+        echo " FAIL"
+        echo "Creating remote directory \"$REMEMBER_DEST\" failed. Abort."
+        exit 1
+    fi
+fi
+echo " OK"
+
+echo -n "Check remote destination directory (previous)..."
+REMEMBER_LINKDEST=$REMEMBER_BASEPATH/last
+if ssh -p$REMEMBER_ONSITE_REVERSE_PORT $REMEMBER_OFFSITE_USER@localhost "[ ! -d $REMEMBER_LINKDEST ]"; then
+    if ! ssh -p$REMEMBER_ONSITE_REVERSE_PORT $REMEMBER_OFFSITE_USER@localhost "mkdir $REMEMBER_LINKDEST"; then
+        echo " FAIL"
+        echo "Creating remote directory \"$REMEMBER_LINKDEST\" failed. Abort."
+        exit 1
+    fi
+fi
+echo " OK"
+
+echo -n "Checking remote rsync permissions..."
+REMEMBER_RSYNC_PERMISSION=/etc/sudoers.d/01_remember-backup
+if ssh -p$REMEMBER_ONSITE_REVERSE_PORT $REMEMBER_OFFSITE_USER@localhost "[ ! -e $REMEMBER_RSYNC_PERMISSION ]"; then
     echo " FAIL"
-    echo "Missing directory \"$REMEMBER_LINKDEST\". Abort."
+    echo "Missing rsync permission file $REMEMBER_RSYNC_PERMISSION. Manually copy to remote machine. Abort."
     exit 1
 fi
 echo " OK"
 
 echo -n "Running rsync..."
-rsync   --archive --stats --progress \
+if ! rsync   --archive --stats --progress \
         --rsh="ssh -p$REMEMBER_ONSITE_REVERSE_PORT" --rsync-path="sudo rsync" \
         --delete-before --delete-excluded --prune-empty-dirs \
         --link-dest=$REMEMBER_LINKDEST \
-        $REMEMBER_SOURCE $REMEMBER_OFFSITE_USER@localhost:$REMEMBER_DEST
-if (( $? == 0 )); then
-    echo " OK"
-else
+        $REMEMBER_SOURCE $REMEMBER_OFFSITE_USER@localhost:$REMEMBER_DEST; then
+#if (( $? != 0 )); then
     echo " FAIL"
+    exit 1
 fi
+echo " OK"
+
+echo -n "Rotating backups..."
+REMEMBER_ROTATE=$REMEMBER_BASEPATH/remember-rotate.sh
+if ! scp -P$REMEMBER_ONSITE_REVERSE_PORT remember-rotate.sh $REMEMBER_OFFSITE_USER@localhost:$REMEMBER_ROTATE; then
+    echo " FAIL"
+    exit 1
+fi
+
+if ! ssh -p$REMEMBER_ONSITE_REVERSE_PORT $REMEMBER_OFFSITE_USER@localhost "$REMEMBER_ROTATE"; then
+    echo " FAIL"
+    echo "Failed execution of $REMEMBER_ROTATE. Abort."
+    exit 1
+fi
+
+if ! ssh -p$REMEMBER_ONSITE_REVERSE_PORT $REMEMBER_OFFSITE_USER@localhost "rm $REMEMBER_ROTATE"; then
+    echo " FAIL"
+    echo "Failed removal of $REMEMBER_ROTATE. Abort."
+    exit 1
+fi
+echo " OK"
+
+# TODO: encrypt backup
